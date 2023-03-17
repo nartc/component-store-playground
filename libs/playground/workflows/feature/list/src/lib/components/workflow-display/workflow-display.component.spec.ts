74f1c99ba26c4820f76f0f4faf8b59b2c19be540
xocomil <xocomil@gmail.com>
2023-03-16 19:03:17 -0600

    converted to use Spectator and fixed tests

--- libs/playground/workflows/feature/list/src/lib/components/workflow-display/workflow-display.component.spec.ts
+++ libs/playground/workflows/feature/list/src/lib/components/workflow-display/workflow-display.component.spec.ts
@@ -1,22 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { ActivatedRoute } from '@angular/router'
+import { Workflow, WorkflowType } from '@component-store-playground/playground/workflows/data-access'
+import { createComponentFactory, mockProvider } from '@ngneat/spectator/jest'
+import { WorkflowListStore } from '../../stores'
 import { WorkflowDisplayComponent } from './workflow-display.component'
 
 describe('WorkflowDisplayComponent', () => {
-  let component: WorkflowDisplayComponent
-  let fixture: ComponentFixture<WorkflowDisplayComponent>
+  const createComponent = createComponentFactory({
+    component: WorkflowDisplayComponent,
+    mocks: [ActivatedRoute],
+    providers: [
+      mockProvider(WorkflowListStore, {
+        deleteWorkflowEffect: jest.fn(),
+      }),
+    ],
+  })
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [WorkflowDisplayComponent],
-    }).compileComponents()
-
-    fixture = TestBed.createComponent(WorkflowDisplayComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
+  const createWorkflow = ({ id = '1', name = 'foo' }: Partial<Workflow> = {}): Workflow => ({
+    id,
+    name,
+    group: { id: '2', children: [], type: WorkflowType.group, level: 0 },
+    maxDepth: 2,
   })
 
   it('should create', () => {
-    expect(component).toBeTruthy()
+    const spectator = createComponent({ props: { workflow: createWorkflow({ id: '1234' }) } })
+
+    expect(spectator).toBeTruthy()
+  })
+
+  it('should call store.deleteWorkflowEffect on deleteWorkflow', () => {
+    const workflow = createWorkflow({ name: 'Chau' })
+    const spectator = createComponent({ props: { workflow } })
+    const store = spectator.inject(WorkflowListStore)
+
+    spectator.component.deleteWorkflow()
+    expect(store.deleteWorkflowEffect).toHaveBeenCalledWith(workflow)
   })
 })
